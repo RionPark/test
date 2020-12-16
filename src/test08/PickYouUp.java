81b1c6c981aee3c2146bbc2505ada79a9191cce5
Rion Park <redfilaruntime@gmail.com>
2020-12-16 16:54:22 +0900

    머리 아프죠~~? 괜찮아요~ 전 안아프거든요 ^^

--- src/test08/PickYouUp.java
+++ src/test08/PickYouUp.java
@@ -7,9 +7,6 @@ public class PickYouUp {
 	public static void main(String[] args) {
 		Random r = new Random();
 		System.out.println(r.nextInt(16)+1);
-		String str = "123,1";
-		int idx = str.indexOf(",");
-		str = str.substring(idx);
-		System.out.println(str);
+
 	}
 }
